d09b0919a4fa60723ed759a51e912bf4f80f421f
Sami <70750616+sammy2h@users.noreply.github.com>
2021-07-02 13:32:19 +0300

    no event page

---
+++ src/app/components/no-event/no-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-event',
+  templateUrl: './no-event.component.html',
+  styleUrls: ['./no-event.component.css']
+})
+export class NoEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
