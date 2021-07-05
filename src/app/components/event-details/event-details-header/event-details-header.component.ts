84ceb2b5269a6e64a649a381bef6bde770543719
AmroMbd <37118920+AmroMbd@users.noreply.github.com>
2021-07-05 13:56:21 +0300

    display extra info for runs names

--- src/app/components/event-details/event-details-header/event-details-header.component.ts
+++ src/app/components/event-details/event-details-header/event-details-header.component.ts
@@ -14,28 +14,5 @@ export class EventDetailsHeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  returnSecondPartRunName(run:any, marketName:string){
-
-    //*ngIf="currentSport" in detail to above level
-    
-    let showSign = marketName.toLowerCase().includes('handicap');
-
-    if(run.handicap){
-      let num = +run.handicap;
-      if(num.toString().includes('.75') || num.toString().includes('.25')){
-        if(!num.toString().includes('-') && showSign){
-          return '+'+(num +0.25) + ' & ' + '+'+(num -0.25)
-        } else{
-          return (num +0.25) + ' & ' + (num -0.25)
-        }
-      }else{
-        return num;
-      }
-    }
-    else{
-      return '';
-    }
-  }
   
 }
