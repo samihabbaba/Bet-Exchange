33a20a2b4abefa1e84270349ff4219d1614966c2
Sami <70750616+sammy2h@users.noreply.github.com>
2021-07-05 15:49:09 +0300

    structure of bet slip

--- src/app/components/layout/bet-slip/bet-slip.component.ts
+++ src/app/components/layout/bet-slip/bet-slip.component.ts
@@ -1,19 +1,43 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-bet-slip',
   templateUrl: './bet-slip.component.html',
-  styleUrls: ['./bet-slip.component.css']
+  styleUrls: ['./bet-slip.component.css'],
 })
 export class BetSlipComponent implements OnInit {
+  selectedBets: any[] = [
+    // {name:'1st'},{name:'2nd'}
+  ];
+  currentOpenBets: any[] = [];
+  tabToDisplay: string = 'Singles';
+  @ViewChild('singles') singlesTab?: ElementRef;
+  @ViewChild('openBets') openBetsTab?: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   fontAwesomeIcons = {
-    exit: faTimes
+    exit: faTimes,
+  };
+
+  resetBets() {
+    this.selectedBets = [];
+  }
+
+  submitBets() {}
+
+  handleTabClick(event: any) {
+    if (event.target.textContent !== this.tabToDisplay) {
+      this.singlesTab?.nativeElement.classList.remove('active');
+      this.openBetsTab?.nativeElement.classList.remove('active');
+      event.target.textContent == 'Singles'
+        ? this.singlesTab?.nativeElement.classList.add('active')
+        : this.openBetsTab?.nativeElement.classList.add('active');
+
+      this.tabToDisplay = event.target.textContent;
+    }
   }
 }
