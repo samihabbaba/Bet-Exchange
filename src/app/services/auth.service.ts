23a5b3660e06d0f51c807f0c3c8c14527372b2b1
AmroMbd <37118920+AmroMbd@users.noreply.github.com>
2021-09-07 11:09:21 +0300

    fix open bet

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -245,7 +245,8 @@ export class AuthService {
     }
 
     this.dataService.getBets(1,5000,this.decodedToken.id,'','','','','','','','',false,'','UNSETTLED','','').subscribe(resp =>{
-      this.betSlipService.currentOpenBets = resp.body.items;
+
+      this.betSlipService.currentOpenBets = resp.body.items.filter((x:any)=> x.status == 'UNMATCHED' ||  x.status == 'PENDING')
       this.betSlipService.updateOpenBetsOptions();
     },
     error =>{
