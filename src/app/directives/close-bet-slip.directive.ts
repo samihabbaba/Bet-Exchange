bfd74523772ab961e89d3741171027f87511e063
Sami Habbaba <sami.software@hotmail.com>
2021-09-09 14:34:31 +0300

    some style & bug fixes

--- src/app/directives/close-bet-slip.directive.ts
+++ src/app/directives/close-bet-slip.directive.ts
@@ -15,12 +15,16 @@ export class CloseBetSlipDirective {
   @Input() displayBetSlip?: any;
   @Output() closeBetSlip: EventEmitter<any> = new EventEmitter();
 
+  overlayPanel: any = document.querySelector('.cdk-overlay-container');
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('document: click', ['$event']) clickOutOfMenu(event: any) {
     if (
       !this.elRef.nativeElement.contains(event.target) &&
-      this.displayBetSlip
+      this.displayBetSlip &&
+      !event.target.classList.contains('mat-option') &&
+      !event.target.classList.contains('mat-option-text')
     ) {
       this.closeBetSlip.emit();
     }
